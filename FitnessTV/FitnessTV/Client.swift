c75aba546c0ba8ae469320002e3ce91769a50943
Alex Stroulger <alexstroulger@gmail.com>
2016-04-30 08:46:16 -0400

    Creates Initial storyboard
    Adds Client for fetching videos
    
    * Adds plist for keys

---
+++ FitnessTV/FitnessTV/Client.swift
@@ -0,0 +1,40 @@
+
+import Foundation
+
+class Client: NSObject {
+    
+    func retrieveKeyFromPList() -> String? {
+        let pathOfPlist = NSBundle.mainBundle().pathForResource("keys", ofType: "plist")
+        if let path = pathOfPlist {
+            do {
+                return try String.init(contentsOfFile: path)
+            }
+            catch {
+               print("no path!")
+            }
+        }
+        return nil
+    }
+    
+    func performGetRequest(targetURL: NSURL!, completion: (data: NSData?, HTTPStatusCode: Int, error: NSError?) -> Void) {
+        let request = NSMutableURLRequest(URL: targetURL)
+        request.HTTPMethod = "GET"
+        
+        let sessionConfiguration = NSURLSessionConfiguration.defaultSessionConfiguration()
+        
+        let session = NSURLSession(configuration: sessionConfiguration)
+        
+        let task = session.dataTaskWithRequest(request) { (data: NSData?, response: NSURLResponse?, error: NSError?) in
+            dispatch_async(dispatch_get_main_queue(), {
+                var statusCode = 0
+                if let responseValue : NSHTTPURLResponse = response as? NSHTTPURLResponse {
+                    statusCode = responseValue.statusCode
+                }
+                completion(data: data, HTTPStatusCode: statusCode, error: error)
+            })
+        }
+        
+        task.resume()
+    }
+    
+}
