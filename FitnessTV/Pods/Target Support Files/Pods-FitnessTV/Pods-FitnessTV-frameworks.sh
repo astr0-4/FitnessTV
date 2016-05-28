26925d312915aec215b69b80e2a5a955cd348fa0
Alex Stroulger <alexstroulger@gmail.com>
2016-05-28 17:24:08 -0400

    Updates pods

--- FitnessTV/Pods/Target Support Files/Pods-FitnessTV/Pods-FitnessTV-frameworks.sh
+++ FitnessTV/Pods/Target Support Files/Pods-FitnessTV/Pods-FitnessTV-frameworks.sh
@@ -84,8 +84,10 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "Pods-FitnessTV/AFNetworking.framework"
   install_framework "Pods-FitnessTV/XCDYouTubeKit.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "Pods-FitnessTV/AFNetworking.framework"
   install_framework "Pods-FitnessTV/XCDYouTubeKit.framework"
 fi
